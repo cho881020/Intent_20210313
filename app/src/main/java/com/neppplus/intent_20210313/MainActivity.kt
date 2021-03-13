7c2667f8117e4100245333f0aa29c12e3d66d916
코딩티처지니 <kj_cho@idealidea.co.kr>
2021-03-13 12:43:22 +0900

    일단 단순 이동으로 구현

--- app/src/main/java/com/neppplus/intent_20210313/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_20210313/MainActivity.kt
@@ -20,5 +20,26 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        moveToSecondBtn.setOnClickListener {
+//            화면 이동인건 동일. => Intent 기초 사용법은 그대로 적용.
+
+//            비행기 티켓
+            val myIntent = Intent(this, MySecondActivity::class.java)
+
+//            실제 탑승
+            startActivity(myIntent)
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
