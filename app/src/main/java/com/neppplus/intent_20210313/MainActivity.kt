625a31c37dc4c131e78ebb09abc58417525c19d8
코딩티처지니 <kj_cho@idealidea.co.kr>
2021-03-13 14:46:30 +0900

    CALL 기능 활용 예제   - 권한 처리 하지 않아서 앱이 죽는다.

--- app/src/main/java/com/neppplus/intent_20210313/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_20210313/MainActivity.kt
@@ -65,6 +65,14 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        callBtn.setOnClickListener {
+            val phoneNum = phoneNumEdt.text.toString()
+
+            val myUri = Uri.parse("tel:${phoneNum}")
+            val myIntent = Intent(Intent.ACTION_CALL, myUri)
+            startActivity(myIntent)
+        }
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
